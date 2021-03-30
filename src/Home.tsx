6b3ff42b2fafaa36486c2d1b86345beb0aec1b8e
Heartin Kanikathottu <heartin@gmail.com>
2021-03-30 20:18:46 +0530

    First commit

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,7 @@
 import React, {FC} from "react";
 
 const Home: FC = () => {
-    return <div> Hello World! First Sample React Home Project</div>
+    return <div> Hello World! First Sample React Home Project-Test</div>
 };
 
 export default Home;
